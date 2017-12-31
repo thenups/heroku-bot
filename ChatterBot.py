d44b6b1b2590eb054b923a4017d1acb3ae4a0eea
thenups <nn.mathur@gmail.com>
2017-12-31 13:46:52 -0800

    update countdown for twitter bot

--- ChatterBot.py
+++ ChatterBot.py
@@ -2,14 +2,13 @@
 import tweepy
 import json
 import time
-import time
 
 
 # Twitter API Keys
 consumer_key = "7TlA9v2GbHF3ULBa3IcJMOFHB"
 consumer_secret = "6ONQkhVXU5bP2DVOIpk0ygt6RXSZcrFbRjUDpF73IhfjGgl0qN"
-access_token = "944048582826397696-hFW38T3AEloUaxEZv9er58Ch804HFLJ"
-access_token_secret = "wcfyskPPMvNfwGrN6awRiW1hv1XNP9NYOM2iYHqk22j29"
+access_token = "944048582826397696-K562QtFCN9Re9I30ksQyiR6xvZj7XHa"
+access_token_secret = "0o7PG1syI2uEruYbWYNDNiQ2eFGlcEUdOQAHJ698DzYfv"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -18,13 +17,10 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 # Create a function that tweets
-def tweetOut(tweetCount):
-    try:
-        api.update_status('This is tweet #{}, but with a timer man!'.format(tweetCount))
-    except:
-        millis = int(round(time.time() * 1000))
-        api.update_status('Tried to post duplicate {}'.format(millis))
+def tweetOut():
+    currentsec = int(time.time())
+    api.update_status('It has been {:,} seconds since the epoch (1 Jan 1970)'.format(currentsec))
 
 for x in range(5):
-    tweetOut(x)
+    tweetOut()
     time.sleep(6)
